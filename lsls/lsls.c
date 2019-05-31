2b97e6821d76abf5fb9b911c5b248d39d4156b3b
Kingaa6 <Kingaa6@gmail.com>
2019-05-31 10:20:02 -0700

    Add parser to command line and loop for args in parsed array

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -2,40 +2,89 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <string.h>
 
+#define MAX_TOKENS 100
 #define PROMPT "shell$ "
 /**
  * Main
  */
+
+// concat function from SO
+char *concat(const char *s1, const char *s2)
+{
+  char *result = malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
+  // in real code you would check for errors in malloc here
+  strcpy(result, s1);
+  strcat(result, s2);
+  return result;
+}
+
 int main(int argc, char **argv)
 {
-  // Parse command line
+  // Parse command line to split each level
+  char *args[MAX_TOKENS];
+  char *token;
+
+  int count = 0;
+
+  token = strtok(argv[1], " \t\n\r");
+
+  while (token != NULL)
+  {
+    // put the token in the args array
+    args[count] = token;
+    (count)++;
+    token = strtok(NULL, " /");
+  }
+
+  // add a NULL to the end of the args array to indicate that there are no more args
+  args[count] = NULL;
 
   // Open directory
   DIR *dir;
   struct dirent *d;
+  struct stat buf;
 
-  char user_input[20];
-
-  // default to open current dir if no input provided
-  if (argv[1] == NULL)
+  if (count == 0)
   {
     dir = opendir(".");
+    while ((d = readdir(dir)) != NULL)
+    {
+      stat(d->d_name, &buf);
+      printf("%lld", buf.st_size);
+      printf("  %s\n", d->d_name);
+    }
+    // Close directory
+    closedir(dir);
   }
-  else if ((dir = opendir(argv[1])) == NULL)
+  else
   {
-    printf("Error occurred.");
-    exit(1);
-  }
+    for (int i = 0; i < count; i++)
+    {
+      // for each directory in parsed, open dir
+      if ((dir = opendir(args[i])) == NULL)
+      {
+        printf("Error occurred. Please enter a valid directory.\n");
+        exit(1);
+      }
 
-  // prints everything in directory
-  while ((d = readdir(dir)) != NULL)
-  {
-    printf("  %s\n", d->d_name);
+      if (dir)
+      {
+        // prints everything in directory
+        while ((d = readdir(dir)) != NULL)
+        {
+          char *full_path = concat(argv[1], d->d_name);
+          // run concat function
+          stat(full_path, &buf);
+          printf("%lld", buf.st_size);
+          printf("  %s\n", d->d_name);
+        }
+        // Close directory
+        closedir(dir);
+      }
+    }
   }
-
-  // Close directory
-  closedir(dir);
 
   return 0;
 }
