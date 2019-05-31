761977a6546ccd761624e36104ef37aa52d39a46
Kingaa6 <Kingaa6@gmail.com>
2019-05-31 08:53:21 -0700

    Add code to print current dir if no arg is provided

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -11,31 +11,18 @@ int main(int argc, char **argv)
 {
   // Parse command line
 
-  int i;
-
-  printf("There are %d command line argument(s):\n", argc);
-
-  for (i = 0; i < argc; i++)
-  {
-    printf("   %s\n", argv[i]);
-  }
-
   // Open directory
   DIR *dir;
   struct dirent *d;
 
-  // default to open current dir if no input provided
-  // not yet working
-  if (!argv[1])
-  {
-    if ((dir = opendir(".")) == NULL)
-    {
-      printf("Error occurred.");
-      exit(1);
-    }
-  }
+  char user_input[20];
 
-  if ((dir = opendir(argv[1])) == NULL)
+  // default to open current dir if no input provided
+  if (argv[1] == NULL)
+  {
+    dir = opendir(".");
+  }
+  else if ((dir = opendir(argv[1])) == NULL)
   {
     printf("Error occurred.");
     exit(1);
