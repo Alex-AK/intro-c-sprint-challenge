abe8332d4bf534b8ed9e8400be939fe1a4136cf6
Kingaa6 <Kingaa6@gmail.com>
2019-05-31 08:34:24 -0700

    Add solution code for opening specified dir

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,6 +1,9 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
+#define PROMPT "shell$ "
 /**
  * Main
  */
@@ -8,11 +11,35 @@ int main(int argc, char **argv)
 {
   // Parse command line
 
+  printf("%s\n", PROMPT);
+
   // Open directory
+  DIR *dir;
+  struct dirent *d;
 
-  // Repeatly read and print entries
+  // default to open current dir if no input provided
+  // not yet working
+  if (!argv[1])
+  {
+    if ((dir = opendir(".")) == NULL)
+    {
+      printf("Error occurred.");
+      exit(1);
+    }
+  }
 
-  // Close directory
+  // opens specified dir
+  if ((dir = opendir(argv[1])) == NULL)
+  {
+    printf("Error occurred.");
+    exit(1);
+  }
+
+  // prints everything in directory
+  while ((d = readdir(dir)) != NULL)
+  {
+    printf("  %s\n", d->d_name);
+  }
 
   return 0;
 }
